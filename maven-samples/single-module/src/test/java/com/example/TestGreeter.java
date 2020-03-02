fac241891a8a013c1bb8eff84b3be3194737d503
Andre CHAGAS <andre.chagas@tvfsoftware>
2020-03-02 23:26:17 +0000

    Commit at Mon Mar  2 23:26:17 UTC 2020

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,4 +42,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+   
+   @Test
+  public void greetShouldIncludeMessages() {
+    String someone = "Test4";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
