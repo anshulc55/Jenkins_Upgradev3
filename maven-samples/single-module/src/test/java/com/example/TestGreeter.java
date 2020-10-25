87f40e6bddcec58408382c7a0b3d77033e815290
jogindersingh1913 <49472802+jogindersingh1913@users.noreply.github.com>
2020-10-25 14:57:20 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+    @Test
+  public void greetShouldIncludeGreetingMessage2() {
+    String someone = "World, Thanks you making me Happy by integrating jenkins";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }  
 }
