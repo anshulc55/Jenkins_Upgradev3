bef0a518847c7d77e6b36b072929a5d3319dd844
MR V KAIKATTI SUGUMARAN <vijayfromnamakkal@gmail.com>
2020-12-06 12:27:59 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
+    String someone = "World!";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
