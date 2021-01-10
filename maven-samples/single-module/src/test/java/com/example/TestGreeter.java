65c1f7030de3e6a587eebeaba3ae5360094914d1
Fasishan <73315253+Fasishan@users.noreply.github.com>
2021-01-10 13:57:02 +0530

    Just added massage
    World, Thanks you making me more Happy then before";

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thanks you making me more Happy then before";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
