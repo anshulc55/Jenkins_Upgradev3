1b4122462178641f693765ea13075f20ce378c51
Anshul Chauhan <anshulc55@gmail.com>
2021-08-06 23:15:41 +0530

    Maven project Fix Commit

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -16,4 +16,9 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
+
+  public static void main(String args[]){
+    
+	  System.out.println("Hello from Main Method!!!");
+  }
 }
