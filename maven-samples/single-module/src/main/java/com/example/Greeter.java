9745f8e7df97c9799877b8c99b0f8aa902343a3f
gitcloudvipin <92628442+gitcloudvipin@users.noreply.github.com>
2021-10-17 12:10:18 +0530

    Update Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,6 +21,6 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method!!!");
+	  System.out.println("Hello from Main Method vipin!!!");
   }
 }
