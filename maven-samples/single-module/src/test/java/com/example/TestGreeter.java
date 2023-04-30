f5408b534a2d5cd56ae56908903cd8c33fbaf995
Anshul Chauhan <anshulc55@gmail.com>
2023-04-30 12:36:39 +0530

    Test Failed

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -8,7 +8,7 @@ import org.mockito.internal.matchers.LessThan;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class TestGreeter {
