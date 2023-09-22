01645a1923d7a2c5e0d38e11a4ce05955fd38e33
Anshul Chauhan <anshulchauhan@Anshuls-MacBook-Pro.local>
2023-09-23 00:30:03 +0530

    Std Jenkins

---
+++ jenkins-plugin-model/src/Pi.Wcf/IPiService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Pi.Wcf
+{
+    [ServiceContract]
+    public interface IPiService
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        string Pi(string dp);
+    }
+}
---
+++ jenkins-plugin-model/src/Pi.Wcf/PiService.svc.cs
@@ -0,0 +1,27 @@
+﻿using Pi.Math;
+using System.Configuration;
+
+namespace Pi.Wcf
+{
+    public class PiService : IPiService
+    {
+        private static readonly int _DefaultDp = 6;
+        private static readonly bool _MetricsEnabled;
+
+        static PiService()
+        {
+            _MetricsEnabled = bool.Parse(ConfigurationManager.AppSettings["Computation:Metrics:Enabled"]);
+        }
+
+        public string Pi(string dp)
+        {
+            int.TryParse(dp, out int actualDp);
+            if (actualDp < 1)
+            {
+                actualDp = _DefaultDp;
+            }
+            var pi = MachinFormula.Calculate(actualDp, _MetricsEnabled);
+            return pi.ToString();
+        }
+    }
+}
