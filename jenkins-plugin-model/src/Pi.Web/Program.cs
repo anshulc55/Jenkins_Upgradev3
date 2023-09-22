c7dbf1f11e62a93ba5387402c41bee918ca33c4d
Anshul Chauhan <anshulc55@gmail.com>
2023-09-23 03:08:33 +0530

    Update Program.cs

--- jenkins-plugin-model/src/Pi.Web/Program.cs
+++ jenkins-plugin-model/src/Pi.Web/Program.cs
@@ -1,3 +1,4 @@
+User
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Pi.Math;
@@ -6,32 +7,48 @@ using PowerArgs;
 using System;
 using System.IO;
 
-var arguments = Args.Parse<Arguments>(args);
-switch (arguments.Mode)
+namespace Pi.Web
 {
-    case RunMode.Web:
-        Console.WriteLine("Running in web mode...");
-        CreateHostBuilder(args).Build().Run();
-        break;
-
-    case RunMode.Console:
-        Console.WriteLine(GetPi(arguments.DecimalPlaces));
-        break;
-
-    case RunMode.File:
-        File.WriteAllText(arguments.OutputPath, GetPi(arguments.DecimalPlaces));
-        Console.WriteLine($"Wrote pi to: {arguments.DecimalPlaces} dp; at: {arguments.OutputPath}");
-        break;
-}
-
-string GetPi(int decimalPlaces)
-{
-    return MachinFormula.Calculate(decimalPlaces).ToString();
-}
-
-IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            webBuilder.UseStartup<Startup>();
-        });
+            try
+            {
+                var arguments = Args.Parse<Arguments>(args);
+                switch (arguments.Mode)
+                {
+                    case RunMode.Web:
+                        Console.WriteLine("Running in web mode...");
+                        CreateHostBuilder(args).Build().Run();
+                        break;
+
+                    case RunMode.Console:
+                        Console.WriteLine(GetPi(arguments.DecimalPlaces));
+                        break;
+
+                    case RunMode.File:
+                        File.WriteAllText(arguments.OutputPath, GetPi(arguments.DecimalPlaces));
+                        Console.WriteLine($"Wrote pi to: {arguments.DecimalPlaces} dp; at: {arguments.OutputPath}");
+                        break;
+                }
+            }
+            catch (ArgException)
+            {
+                Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<Arguments>());
+            }
+        }
+
+        private static string GetPi(int decimalPlaces)
+        {
+            return MachinFormula.Calculate(decimalPlaces).ToString();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
