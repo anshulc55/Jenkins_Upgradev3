cb7fad887cc3d1bf99fc096fef5cb179713d63cc
Anshul Chauhan <anshulc55@gmail.com>
2023-09-23 20:09:27 +0530

    Update Program.cs

--- jenkins-plugin-model/src/Pi.Math/Program.cs
+++ jenkins-plugin-model/src/Pi.Math/Program.cs
@@ -1,17 +1,10 @@
 using System;
-using Pi.Math; 
+using Pi.Math; // Import the necessary namespaces
 
-public class Program
-{
-    public static void Main()
-    {
-        int decimalPlaces = 100; // Set the number of decimal places you want
-        bool recordMetrics = true; // Set whether to record metrics or not
+int decimalPlaces = 100; // Set the number of decimal places you want
+bool recordMetrics = true; // Set whether to record metrics or not
 
-        // Calculate Pi using your MachinFormula class
-        HighPrecision pi = MachinFormula.Calculate(decimalPlaces, recordMetrics);
+HighPrecision pi = MachinFormula.Calculate(decimalPlaces, recordMetrics);
 
-        // Print the result
-        Console.WriteLine($"Pi to {decimalPlaces} decimal places: {pi}");
-    }
-}
+// Print the result
+Console.WriteLine($"Pi to {decimalPlaces} decimal places: {pi}");
