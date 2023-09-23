cde44d484f133cb265f0ef43587b73bf43bdde50
Anshul Chauhan <anshulchauhan@Anshuls-MacBook-Pro.local>
2023-09-23 21:25:30 +0530

    Docker pipeline

--- jenkins-plugin-model/ci/04-push.sh
+++ jenkins-plugin-model/ci/04-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker image build -t anshuldevops/anshuldevops/jenkins-demo2:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
+docker image build -t anshuldevops/jenkins-demo2:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
 
 if [ -z ${DOCKER_HUB_USER+x} ]
 then 
@@ -8,4 +8,4 @@ else
     docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 fi
 
-docker push anshuldevops/anshuldevops/jenkins-demo2:$0
+docker push anshuldevops/jenkins-demo2:$0
